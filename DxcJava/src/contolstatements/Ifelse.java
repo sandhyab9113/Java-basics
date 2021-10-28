e61d4c608327f6bd2bfa90586b399283733e6b0b
Sandhya B <Sandhya B@DESKTOP-34S5LRG>
2021-10-28 00:17:30 -0700

    control statement

---
+++ DxcJava/src/contolstatements/Ifelse.java
@@ -0,0 +1,15 @@
+package contolstatements;
+
+public class Ifelse {
+	public static void main(String[] args) {
+		int number = 13;
+
+		if(number %2 == 0) {
+			System.out.println("even no");
+		}
+		else {
+			System.out.println("odd no");
+		}
+	}
+
+}
